447563611474f9f42786d4480284e0a43d180b4f
Raymond Nowley <nowley.r@gmail.com>
2021-08-19 19:05:56 +1200

    Added a table

---
+++ sql-scripts/Payee.sql
@@ -0,0 +1,9 @@
+if not exists(select 1 from sys.tables where object_id = object_id(N'dbo.Payee'))
+begin
+
+	create table dbo.Payee (
+		payeeId int primary key identity(1, 1),
+		description nvarchar(50),
+		code nvarchar(50)
+	);
+end
